180d5b0550e74a9174c6d134b38f28d84f9b88e9
GhostMobin <s1810677110@ru.ac.bd>
2023-09-24 10:23:04 +0600

    Loader

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
+import PuffLoader from "react-spinners/PuffLoader";
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header></Header>
-
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? (
+        <PuffLoader className=" mt-64 mx-auto" color="white" />
+      ) : (
+        <Outlet></Outlet>
+      )}
     </>
   );
 };
